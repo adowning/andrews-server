4ab9001df6a2a5729da9c0499404122ce1889c02
Ash Downing <ashdowning@gmail.com>
2020-03-13 23:58:42 -0500

    asdf

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import logger from 'parse-server/lib/logger'
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 const StackUtils = require('stack-utils')
 const stack = new StackUtils({ cwd: process.cwd(), internals: StackUtils.nodeInternals() })
 console.log(stack.clean(new Error().stack))
 // console.log(new Error())
 
-if (process.env.NODE_ENV === 'development') {
-  dotenv.config({ path: '.env.dev' })
-} else {
-  dotenv.config({ path: '.env.prod' })
-}
+// if (process.env.NODE_ENV === 'development') {
+//   dotenv.config({ path: '.env.dev' })
+// } else {
+//   dotenv.config({ path: '.env.prod' })
+// }
 // eslint-disable-next-line import/first
 import { App, InitClasses } from './app'
 
